5c067648dbfb1c76e80b2b90a2fbb3b5d73362b6
Salinn <pmd6624@rit.edu>
2013-12-28 16:42:10 -0500

    updated a few things related to user and admin pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def after_sign_in_path_for(resource)
-    user_home_path
+    if current_user.admin
+      admin_home_path
+    else
+      user_home_path
+    end
   end
 
 end
