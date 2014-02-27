58686dcba81a4783a73d6eca0d291d1f313ddb31
Paul Darragh <pmd6624@rit.edu>
2014-02-26 20:54:52 -0500

    added in table of bikes tha can be checked out, filters by if a bike is checked out/not broken

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -24,13 +24,28 @@ class StaticPagesController < ApplicationController
   def quiz
   end
 
-  def user_home
+  def home_user
   end
 
-  def admin_home
+  def home_admin
+    @checked_outs = not_cheched_in
+    @can_checked_outs = can_checked_out
   end
 
   def reports
     @bikes = Bike.all
   end
+end
+
+def can_checked_out
+  can_checked_outs = Bike.order(:bike_id).find_all_by_checked_out(false)
+  can_checked_outs.each do |check_out|
+    if check_out.need_repair or check_out.addtional_repair_need or check_out.passed_inspection == false
+      can_checked_outs.delete check_out
+    end
+  end
+
+end
+def not_cheched_in
+  CheckedOut.order(:bike_id).find_all_by_time_of_checkin(nil)
 end
--- app/helpers/checked_outs_helper.rb
+++ app/helpers/checked_outs_helper.rb
@@ -1,5 +1,2 @@
 module CheckedOutsHelper
-  def not_cheched_in
-    CheckedOut.order(:bike_id).find_all_by_time_of_checkin(nil)
-  end
 end
