d4d2ae13d8780e2d37e597e3661e8446dd8e7a69
Paul Darragh <pmd6624@rit.edu>
2014-04-07 12:16:24 -0400

    updated total bikes display

--- app/helpers/bikes_helper.rb
+++ app/helpers/bikes_helper.rb
@@ -3,7 +3,7 @@ module BikesHelper
     total__number_of_bikes = 0
     bikes = Bike.all
     bikes.each do |bike|
-      total__number_of_bikes+=1 unless bike.checked_out or bike.need_repair
+      total__number_of_bikes+=1 unless bike.checked_out or bike.need_repair or bike.addtional_repair_need or !bike.passed_inspection
     end
     total__number_of_bikes
   end
